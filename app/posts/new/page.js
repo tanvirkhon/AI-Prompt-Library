b1b95ec23cb92f665bba3260f2e220f66c730401
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-05 23:28:20 -0500

    Created posts folder and files

---
+++ app/posts/new/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Will be rendered at /posts/new
+
+const page = () => {
+  return (
+    <div>New Posts</div>
+  )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Will be rendered at /posts
+const page = () => {
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default page
