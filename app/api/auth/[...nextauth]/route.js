ea7e733fcbe54d78ba4ef9b328a00d14a2c68922
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-07 22:08:28 -0500

    Working on Auth file

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth/next";
+
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+
+// Create Handler
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };
