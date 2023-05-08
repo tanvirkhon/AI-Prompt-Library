47cd323015f948c424c562a7025761b03b0a7534
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-07 23:15:18 -0500

    Added authetication code

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,50 @@
 import NextAuth from "next-auth/next";
-
 import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 import { signIn } from "next-auth/react";
 
 // Create Handler
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // Check if user exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // If not create user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
